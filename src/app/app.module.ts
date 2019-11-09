5c7e5c3d1d8ac03ecc0ca7b6c99863e481b88aa9
adrijfer18 <adrianfernandezj@gmail.com>
2019-11-09 17:15:12 -0300

    Coneccion de a Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { MainComponent } from './pages/main/main.component';
 import { FormComponent } from './pages/form/form.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    FormComponent
+    FormComponent,
+    AngularFireModule,
+    AngularFirestoreModule,
+
+    ReactiveFormsModule
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFirestoreModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
