dd06bd2c9c744129169e1ef7193d627ab1fbbc25
adrijfer18 <adrianfernandezj@gmail.com>
2019-11-09 15:09:08 -0300

    maquetado de header y main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MainComponent } from './pages/main/main.component';
+import { FormComponent } from './pages/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
